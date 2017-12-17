ebc6cda9b48ec88465ccbc7678a8a440ddbd5fdf
vianneyribeiro <vianneyribeiro@gmail.com>
2017-12-16 21:28:47 -0300

    Parque Aquatico Activity

--- app/src/main/java/com/example/vianneyribeiro/nossafortaleza/ParqueAquaticoActivity.java
+++ app/src/main/java/com/example/vianneyribeiro/nossafortaleza/ParqueAquaticoActivity.java
@@ -3,6 +3,7 @@ package com.example.vianneyribeiro.nossafortaleza;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import java.util.ArrayList;
 
@@ -12,6 +13,17 @@ public class ParqueAquaticoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_parque_aquatico);
+
+
+        ListView listView = (ListView) findViewById(R.id.listaParqueAquatico);
+
+        ArrayList<LocalizationItem> arrayList = new ArrayList<LocalizationItem>();
+        arrayList.add(new LocalizationItem(R.drawable.beachpark, "Beach Park", "Beach Park é um complexo turístico do litoral do Nordeste do Brasil, na praia de Porto das Dunas", "16 Km de Fortaleza"));
+        arrayList.add(new LocalizationItem(R.drawable.ytacaranhalowres, "Ytacaranha Park", "O Ytacaranha Park é um complexo turístico que dispõe de um maravilhoso parque aquático e um confortável Hotel de Praia", "220 Km de Fortaleza"));
+
+
+        ArrayAdapter adapter = new LocalizationAdapter(this, arrayList);
+        listView.setAdapter(adapter);
 
 
     }
