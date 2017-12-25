9b8d92a385d14a5aa8fbcc08cf23cbc2600c3b69
vianneyribeiro <vianneyribeiro@gmail.com>
2017-12-25 21:54:19 -0200

    Strings das praias movidos para o strings.xml

--- app/src/main/java/com/example/vianneyribeiro/nossafortaleza/PraiasActivity.java
+++ app/src/main/java/com/example/vianneyribeiro/nossafortaleza/PraiasActivity.java
@@ -17,10 +17,10 @@ public class PraiasActivity extends AppCompatActivity {
         ListView listView = findViewById(R.id.listPraias);
 
         ArrayList<LocalizationItem> arrayList = new ArrayList<>();
-        arrayList.add(new LocalizationItem(R.drawable.praia_de_iracema, "Praia de Iracema", "As águas claras da praia de Iracema são poluídas, mas seu calçadão está sempre cheio de gente correndo, caminhando ou apenas contemplando o mar de ondas fortes. A grande quantidade de bares e de restaurantes garante burburinho dia e noite, mas é à tardinha que o movimento aumenta, principalmente na Ponte Metálica (ou dos Ingleses), uma plataforma que avança mar adentro e local perfeito para se despedir do sol.", "6 Km do Centro"));
-        arrayList.add(new LocalizationItem(R.drawable.praia_do_futuro, "Praia do Futuro", "Preferida dos banhistas, a praia do Futuro ocupa oito dos 25 quilômetros da orla da capital,com direito a areias fofas e claras, ondas fortes e dunas. Limpa e bonita, abriga ao longo de seu calçadão diversas barracas que oferecem duchas de água doce, cadeiras de praia e delícias da gastronomia regional. ", "11 Km do Centro"));
-        arrayList.add(new LocalizationItem(R.drawable.praia_do_meireles, "Praia do Meireles", "Bastante familiar, a praia de Meireles é arborizada, tem calçadões, feira de artesanato e quadra de esportes. O movimento de turistas é grande, uma vez que a maioria dos hotéis está por lá. Com formações rochosas e mar poluído, não é indicada para banhos.", "10 Km do Centro"));
-        arrayList.add(new LocalizationItem(R.drawable.praia_do_mucuripe, "Praia do Mucuripe", "A tranquila enseada de Mucuripe é reduto da comunidade caiçara em pleno centro urbano. Tem areias claras e fofas, coqueiros, quadras de esportes e muitas jangadas que lá aportam repletas de lagostas. Um bom programa no final da tarde é beliscar nas barracas do Mercado do Peixe, onde estão os pescados mais frescos de Fortaleza.", "15 Km do Centro"));
+        arrayList.add(new LocalizationItem(R.drawable.praia_de_iracema, getString(R.string.praia_de_iracema), getString(R.string.desc_praia_de_iracema), getString(R.string.local_praia_do_mucuripe)));
+        arrayList.add(new LocalizationItem(R.drawable.praia_do_futuro, getString(R.string.praia_do_futuro), getString(R.string.desc_praia_do_futuro), getString(R.string.local_praia_do_futuro)));
+        arrayList.add(new LocalizationItem(R.drawable.praia_do_meireles, getString(R.string.praia_do_meireles), getString(R.string.desc_praia_do_meireles), getString(R.string.local_praia_do_meireles)));
+        arrayList.add(new LocalizationItem(R.drawable.praia_do_mucuripe, getString(R.string.praia_do_mucuripe), getString(R.string.desc_praia_do_mucuripe), getString(R.string.local_praia_do_mucuripe)));
 
         ArrayAdapter adapter = new LocalizationAdapter(this, arrayList);
         listView.setAdapter(adapter);
